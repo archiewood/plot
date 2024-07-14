5e270078238e5316bd711e1ee32d5f7305cec1d0
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-07-14 12:28:44 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/hello
+
+go 1.22.5
---
+++ hello.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// check if arguments are passed
+	if len(os.Args) < 2 {
+		fmt.Println("usage: go run main.go [-t title] [labels] , [values] or go run main.go [-t title] [values]")
+		return
+	}
+
+	// initialize variables
+	var title string
+	var labels []string
+	var values []int
+	var valuesArgs []string
+
+	// check if a title flag is passed
+	if os.Args[1] == "-t" {
+		if len(os.Args) < 4 {
+			fmt.Println("usage: go run main.go -t [title] [labels] , [values] or go run main.go -t [title] [values]")
+			return
+		}
+		title = os.Args[2]
+		os.Args = append(os.Args[:1], os.Args[3:]...)
+	}
+
+	// find the separator index
+	sepIndex := -1
+	for i, arg := range os.Args {
+		if arg == "," {
+			sepIndex = i
+			break
+		}
+	}
+
+	if sepIndex != -1 {
+		// parse labels and values if separator is found
+		labels = os.Args[1:sepIndex]
+		valuesArgs = os.Args[sepIndex+1:]
+		if len(labels) != len(valuesArgs) {
+			fmt.Println("the number of labels and values must be the same")
+			return
+		}
+	} else {
+		// parse only values if no separator is found
+		valuesArgs = os.Args[1:]
+	}
+
+	// convert values to integers
+	for _, arg := range valuesArgs {
+		value, err := strconv.Atoi(arg)
+		if err != nil {
+			fmt.Printf("invalid number: %s\n", arg)
+			fmt.Println("usage: go run main.go [labels] , [values] or go run main.go [values]")
+			return
+		}
+		values = append(values, value)
+	}
+
+	// print the title if provided
+	if title != "" {
+		fmt.Println(title)
+	}
+
+	// plot the barchart
+	plot(values, labels)
+}
+
+// plot function to display the barchart
+func plot(values []int, labels []string) {
+	hasLabels := len(labels) > 0
+	for i, value := range values {
+		if hasLabels {
+			label := labels[i]
+			// print the label
+			fmt.Printf("%-7s│", label)
+		} else {
+			fmt.Print("│")
+		}
+		// print the bar
+		for j := 0; j < value; j++ {
+			fmt.Print("■")
+		}
+		// print the value
+		fmt.Printf(" %d\n", value)
+	}
+}
