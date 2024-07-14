a54aee4493be967533c8b656ab4e15e09eec1dff
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-07-14 13:16:23 -0400

    dynamic chart width

--- hello.go
+++ hello.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"os/exec"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -67,12 +70,19 @@ func main() {
 		fmt.Println(title)
 	}
 
+	// get terminal width
+	termWidth, err := getTerminalWidth()
+	if err != nil {
+		fmt.Printf("could not determine terminal width: %s\n", err)
+		return
+	}
+
 	// plot the barchart
-	plot(values, labels)
+	plot(values, labels, termWidth)
 }
 
 // plot function to display the barchart
-func plot(values []int, labels []string) {
+func plot(values []int, labels []string, width int) {
 	hasLabels := len(labels) > 0
 
 	maxLabelLen := 0
@@ -82,19 +92,62 @@ func plot(values []int, labels []string) {
 		}
 	}
 
+	// find the maximum value
+	maxValue := 0
+	for _, value := range values {
+		if value > maxValue {
+			maxValue = value
+		}
+	}
+
+	// find the width available for the bars
+	chartWidth :=
+		width -
+			maxLabelLen -
+			// the length of the value label
+			int(math.Log10(float64(maxValue))+0.5) -
+			// the padding
+			2
+
+	fmt.Print("termWidth:", width, "=\n")
+	for j := 0; j < width; j++ {
+		fmt.Print("■")
+	}
+
+	// calculate the scale factor for the values by dividing the maximum value by the terminal width, accounting for the label length and the value label length
+	scale := float64(maxValue) / float64(chartWidth)
+
 	for i, value := range values {
 		if hasLabels {
 			label := labels[i]
 			// print the label, using the max label length
-			fmt.Printf("% *s │", maxLabelLen, label)
+			fmt.Printf("% *s│", maxLabelLen, label)
 		} else {
 			fmt.Print("│")
 		}
-		// print the bar
-		for j := 0; j < value; j++ {
+
+		// calculate the number of blocks to, rounding the value to the nearest integer
+		scaledValue := int(float64(value)/scale + 0.5)
+
+		for j := 0; j < scaledValue; j++ {
 			fmt.Print("■")
 		}
 		// print the value
 		fmt.Printf(" %d\n", value)
 	}
+}
+
+func getTerminalWidth() (int, error) {
+	cmd := exec.Command("tput", "cols")
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	out, err := cmd.Output()
+	if err != nil {
+		return 0, err
+	}
+	width, err := strconv.Atoi(strings.TrimSpace(string(out)))
+	if err != nil {
+		return 0, err
+	}
+	return width, nil
 }
